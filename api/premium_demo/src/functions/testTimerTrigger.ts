2070acaa19bf24757de791aeebe1167834bde905
小西達大 <konishitatsuhiro@konishitoorudainoMacBook-Pro.local>
2026-08-30 21:23:22 +0900

    feat

--- api/premium_demo/src/functions/testTimerTrigger.ts
+++ api/premium_demo/src/functions/testTimerTrigger.ts
@@ -1,12 +1,14 @@
 import { app, InvocationContext, Timer } from "@azure/functions";
 
 export async function testTimerTrigger(myTimer: Timer, context: InvocationContext): Promise<void> {
-    context.log('Timer function processed request.');
-    context.log(`Timer last ran at: ${myTimer.scheduleStatus?.last}`);
-    context.log(`Timer next run at: ${myTimer.scheduleStatus?.next}`);
+    const serverName = process.env.COMPUTERNAME || 'unknown';
+    const instanceId = process.env.WEBSITE_INSTANCE_ID || 'unknown';
+    context.log(`[Server: ${serverName}] [InstanceId: ${instanceId}] Timer function processed request.`);
+    context.log(`[Server: ${serverName}] Timer last ran at: ${myTimer.scheduleStatus?.last}`);
+    context.log(`[Server: ${serverName}] Timer next run at: ${myTimer.scheduleStatus?.next}`);
 };
 
 app.timer('testTimerTrigger', {
-    schedule: '0 */10 * * * *',
+    schedule: '0 */5 * * * *',
     handler: testTimerTrigger
 });
